6945bd162947578996f051f52c29548ff554f10b
jhanlon2 <jason.hanlon2@gmail.com>
2017-07-21 17:07:22 +0000

    added the rest of the win conditions

--- app.js
+++ app.js
@@ -4,36 +4,75 @@ $(document).ready(function() {
 
 	$('#board').find('.td').on('click', function(e){
 		if(turn_count % 2 === 0) {
-			$(this).text('X')
+			$(this).text('X');
+			checkVictory('X')
 			}
 		else {
-			$(this).text('X')
+			$(this).text('O');
+			checkVictory('O')
 			}
 		turn_count++
 	});
 
-	checkVictory = function() {
-		//horizontal check
+	checkVictory = function(player) {
+		//top horizontal check
 		if($('#board').find('#1').text() !== '') {
 			if($('#board').find('#1').text() = $('#board').find('#2').text()) {
 				if($('#board').find('#2').text() = $('board').find('#3').text()) {
 					alert('Game Over! ' + player + ' is the winner!')
 					}
 				}
-			//diagonal check
+			//left diagonal check
 			if($('#board').find('#1').text() = $('#board').find('#5').text()) {
 				if($('#board').find('#5').text() = $('#board').find('#9').text()) {
 					alert('Game over! '+player+' is the winner!')
 					}
 				}
-			//check vertical
+			//check left vertical
 			if($('#board').find('#1').text() = $('#board').find('#4').text()) {
 				if($('#board').find('#4').text() = $('#board').find('#7').text()) {
 					alert('Game over! '+player+' is the winner!')
 					}
 				}
 			}
-
-		
+		if($('#board').find('#2').text() !== '') {
+			//check middle vertical
+			if($('#board').find('#2').text() = $('#board').find('#5').text()) {
+				if($('#board').find('#5').text() = $('#board').find('#8').text()) {
+					alert('Game over! '+player+' is the winner!')
+					}
+				}
+			}
+		if($('#board').find('#3').text() !== '') {
+			//check right vertical
+			if($('#board').find('#3').text() = $('#board').find('#6').text()) {
+				if($('#board').find('#6').text() = $('#board').find('#9').text()) {
+					alert('Game over! '+player+' is the winner!')
+					}
+				}
+			//rihgt diagonal check
+			if($('#board').find('#3').text() = $('#board').find('#5').text()) {
+				if($('#board').find('#5').text() = $('#board').find('#7').text()) {
+					alert('Game over! '+player+' is the winner!')
+					}
+				}
+			}
+		//middle horizontal check
+		if($('#board').find('#4').text() !== '') {
+			if($('#board').find('#4').text() = $('#board').find('#5').text()) {
+				if($('#board').find('#5').text() = $('#board').find('#6').text()) {
+					alert('Game over! '+player+' is the winner!')
+					}
+				}
+			}
+		//bottom horizontal check
+		if($('#board').find('#7').text() !== '') {
+			if($('#board').find('#7').text() = $('#board').find('#8').text()) {
+				if($('#board').find('#8').text() = $('#board').find('#9').text()) {
+					alert('Game over! '+player+' is the winner!')
+					}
+				}
+			}
 	}
+
 })
